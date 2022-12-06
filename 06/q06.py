bc2b47607109cdbe958a04b8f66637477f3fddeb
Eetu Salli <eetu.salli@gmail.com>
2022-12-06 11:01:20 +0200

    Day 6: shorter solution

--- 06/q06.py
+++ 06/q06.py
@@ -1,22 +1,15 @@
 from aocd import lines
 input = lines[0]
+# input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
-def check_non_repeating_values(array: list):
-    characters = {}
-    for c in array:
-        if c in characters:
-            return False
-        else:
-            characters[c] = 1
-    return True
+def check_non_repeating_values(array):
+    return len(set(array)) == len(array)
 
 def find_marker(line: str, n_unique_chars: int):
-    for i in range(0, len(line)):
-        if i < n_unique_chars:
-            continue
-        found_marker = check_non_repeating_values(line[(i-n_unique_chars):i])
+    for i in range(0, len(line)-n_unique_chars):
+        found_marker = check_non_repeating_values(line[i:i+n_unique_chars])
         if found_marker:
-            return i
+            return i + n_unique_chars
 
 def solve(line, n_unique_chars: int):
     ans = find_marker(line, n_unique_chars)
