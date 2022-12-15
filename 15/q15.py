eec253940662305b7604d89739aee38bddf194c1
Eetu Salli <eetu.salli@gmail.com>
2022-12-15 23:14:23 +0200

    Day 15: not finished

---
+++ 15/q15.py
@@ -0,0 +1,181 @@
+from __future__ import division 
+from shapely.ops import linemerge
+from aocd import lines
+
+test_input = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
+
+test_input = test_input.split("\n")
+
+def Line(p1, p2):
+    A = (p1[1] - p2[1])
+    B = (p2[0] - p1[0])
+    C = (p1[0]*p2[1] - p2[0]*p1[1])
+    return A, B, -C
+
+def get_intersection(L1, L2):
+    D  = L1[0] * L2[1] - L1[1] * L2[0]
+    Dx = L1[2] * L2[1] - L1[1] * L2[2]
+    Dy = L1[0] * L2[2] - L1[2] * L2[0]
+    if D != 0:
+        x = int(Dx / D)
+        y = int(Dy / D)
+        return x,y
+    else:
+        return False
+
+def manhattan_distance(x1, y1, x2, y2):
+    return (abs(x1-x2)+abs(y1-y2))
+
+class Diamond():
+    def __init__(self, input_line: str) -> None:
+        self.sensor = None
+        self.beacon = None
+        self.parse_line(input_line)
+        self.corners = self.get_corners()
+
+    def get_corners(self):
+        corners = []
+        d = manhattan_distance(self.sensor[0], self.sensor[1], self.beacon[0], self.beacon[1])
+        corners.append((self.sensor[0] - d, self.sensor[1]))
+        corners.append((self.sensor[0], self.sensor[1] + d))
+        corners.append((self.sensor[0] + d, self.sensor[1]))
+        corners.append((self.sensor[0], self.sensor[1] - d))
+        return corners
+
+    def parse_line(self, line):
+        sensor_beacon_pair = []
+        reading = False
+        current = ""
+        for c in line:
+            if (not c.isnumeric() and c != '-') and reading:
+                reading = False
+                sensor_beacon_pair.append(int(current))
+                current = ""
+            if reading:
+                current += c
+            if c == "=":
+                reading = True
+        sensor_beacon_pair.append(int(current))
+        self.sensor = (sensor_beacon_pair[0], sensor_beacon_pair[1])
+        self.beacon = (sensor_beacon_pair[2], sensor_beacon_pair[3])
+
+    def get_no_of_beacon_sensor_on_row(self, row):
+        count = 0
+        if self.beacon[1] == row:
+            count += 1
+        if self.sensor[1] == row:
+            count += 1
+        return count
+
+    def get_intersections_with_row(self, row):
+        mid_y = self.sensor[1]
+        top_y = self.corners[1][1]
+        btm_y = self.corners[3][1]
+        #print(btm_y, mid_y, top_y, row)
+        #print(self.corners)
+        intersections = []
+        if row >= mid_y and row <= top_y:
+            L1 = Line(self.corners[0], self.corners[1])
+            LY = Line((0, row), (1, row))
+            L2 = Line(self.corners[1], self.corners[2])
+            R1 = get_intersection(L1, LY)
+            R2 = get_intersection(L2, LY)
+            intersections.append(R1)
+            intersections.append(R2)
+        elif row < mid_y and row >= btm_y:
+            L1 = Line(self.corners[0], self.corners[3])
+            LY = Line((0, row), (1, row))
+            L2 = Line(self.corners[3], self.corners[2])
+            R1 = get_intersection(L1, LY)
+            R2 = get_intersection(L2, LY)
+            
+            intersections.append(R1)
+            intersections.append(R2)
+        else:
+            return None
+        return intersections
+
+def lines_overlap(L1, L2) -> bool:
+    pass
+
+def merge_lines(L1, L2, lines):
+    pass
+
+def handle_overlapping_lines(lines):
+    for line in lines:
+        for compared_line in lines:
+            if line != compared_line:
+                if lines_overlap(line, compared_line):
+                    merge_lines(line, compared_line, lines)
+
+def tuple_tuples_lower_bound(tuple_tuple):
+    return tuple_tuple[0][0]
+
+def get_line_len(line: list):
+    return abs(line[0]-line[1])+1
+
+def get_lines_total_len(lines):
+    total = 0
+    for line in lines:
+        total += get_line_len(line)
+    return total
+
+def get_no_of_beacons_sensors_on_row(arr: 'list[Diamond]', row):
+    beacons_sensors = []
+    for d in arr:
+        s = d.sensor
+        b = d.beacon
+        if d.beacon not in beacons_sensors and d.beacon[1] == row:
+            beacons_sensors.append(d.beacon)
+        if d.sensor not in beacons_sensors and d.sensor[1] == row:
+            beacons_sensors.append(d.sensor)
+    return len(beacons_sensors)
+
+def get_scanned_points_no_on_row(diamonds: 'list[Diamond]', row):
+    lines = []
+    for d in diamonds:    
+        intersections = d.get_intersections_with_row(row)
+        if intersections:
+            lines.append( (intersections[0], intersections[1]) )
+        # else:
+        #     print("None")
+
+    lines.sort(key=tuple_tuples_lower_bound)
+    only_x = list(map(lambda x: [x[0][0], x[1][0]], lines))
+    merged = [only_x[0]]
+    for current in only_x:
+        previous = merged[-1]
+        if current[0] <= previous[1]:
+            previous[1] = max(previous[1], current[1])
+        else:
+            merged.append(current)
+    print(merged)
+    total_on_row = get_lines_total_len(merged) - get_no_of_beacons_sensors_on_row(diamonds, row)
+    print(total_on_row)
+
+
+row = 10
+row = 2000000
+scanned_areas = []
+for line in lines:
+    d = Diamond(line)
+    # intersections = d.get_intersections_with_row(row)
+    # print(d.get_no_of_beacon_sensor_on_row(row))
+    # print(intersections)
+    scanned_areas.append(d)
+
+get_scanned_points_no_on_row(scanned_areas, row)
+# 5068328 wrong
